e9990035f892e082bc14d708668c9729d581e613
AremmyDi <AremmyDi@users.noreply.github.com>
2020-11-18 23:34:17 -0600

    Updated date
    Added formatDate function to load the current temperature by overwriting the HTML.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,39 @@ function displayTemperature (response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let dateElement = document.querySelector("#current-Icon");
+
   console.log(response.data);
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+function formatDate (timestamp) {
+  let date = new Date(timestamp);
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  let day = weekDays[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 
