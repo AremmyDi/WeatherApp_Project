31a8633af587bc19e205d2683231471bcafb5709
AremmyDi <AremmyDi@users.noreply.github.com>
2020-11-18 22:57:34 -0600

    Used the displayTemperature function to add more data.
    Added new data to the WeatherApp by completing the display Temperature function.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 function displayTemperature (response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
+  console.log(response.data);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
+
 
 let apiKey = "30e779d5dfda5389f3e7fff3e46e0d16";
 let city = "Paris";
