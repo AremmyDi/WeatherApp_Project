3d692dd3d395d66922dfc98182a131992a099463
AremmyDi <AremmyDi@users.noreply.github.com>
2020-11-18 22:32:49 -0600

    Tested API using console log
    Added a function to display the temperature, tested it by adding a console.log command. The data is correct.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+function displayTemperature (response) {
+  console.log(response.data.main.temp);
+}
+
 let apiKey = "30e779d5dfda5389f3e7fff3e46e0d16";
 let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
