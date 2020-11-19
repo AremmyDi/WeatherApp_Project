f872de0e4ff5b977032f6344063577ad872a4598
AremmyDi <AremmyDi@users.noreply.github.com>
2020-11-19 00:43:58 -0600

    Search Function added
    Added a new function to search the city. It is still manual, since the form's input still is not being read.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ function displayTemperature (response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#current-Icon");
 
-  console.log(response.data);
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -44,9 +43,11 @@ function formatDate (timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function search (city) {
+  let apiKey = "30e779d5dfda5389f3e7fff3e46e0d16";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "30e779d5dfda5389f3e7fff3e46e0d16";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+search("Paris");
