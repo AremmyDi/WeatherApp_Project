5619a7ad0378f42a5aaf25b4ef3888a6cec4418b
AremmyDi <AremmyDi@users.noreply.github.com>
2020-11-18 23:05:33 -0600

    Added humidity and wind speed data.
    Added humidity and wind speed data inside the displayTemperature function.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ function displayTemperature (response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   console.log(response.data);
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 
